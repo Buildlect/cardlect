41ef33920e6338bd0f8e7941dd78cb869b428c40
El-Sheikh Ibrahim Abba <elsheikhibrahimabba@gmail.com>
2026-03-07 10:04:03 +0100

    real api

--- cardlect-client/lib/api-client.ts
+++ cardlect-client/lib/api-client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/latest',
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://cardlect.buildlect.com/latest',
     headers: {
         'Content-Type': 'application/json',
     },
